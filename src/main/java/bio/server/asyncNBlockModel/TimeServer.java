5eb03be04b23133fa0ba378a3eb87fc9b6eb61d8
Minstrel-jh <635481671@qq.com>
2018-04-25 16:30:57 +0800

    start netty study

---
+++ src/main/java/bio/server/asyncNBlockModel/TimeServer.java
@@ -0,0 +1,41 @@
+package bio.server.asyncNBlockModel;
+
+import bio.handler.TimeServerHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {}
+        }
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port:" + port);
+            Socket socket = null;
+            TimeServerHandlerExecutePool singleExecutor
+                = new TimeServerHandlerExecutePool(50, 10000); // 创建I/O线程池
+
+            while (true) {
+                socket = server.accept(); // 此处会阻塞
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                System.out.println("The time server close");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
---
+++ src/main/java/bio/server/syncBlockModel/TimeServer.java
@@ -0,0 +1,38 @@
+package bio.server.syncBlockModel;
+
+import bio.handler.TimeServerHandler;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {}
+        }
+        ServerSocket server = null;
+
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port:" + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept(); // 此处会阻塞
+                new Thread(new TimeServerHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (server != null) {
+                System.out.println("The time server close");
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
