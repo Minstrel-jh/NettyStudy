2bbe2f46a6df3eeaaea980f5b6db6e316b6ff318
Minstrel-jh <635481671@qq.com>
2018-06-20 20:57:22 +0800

    分隔符解码器

---
+++ src/main/java/frameDecoder/delimiterBase/EchoClientHandler.java
@@ -0,0 +1,34 @@
+package frameDecoder.delimiterBase;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelHandlerInvoker;
+import io.netty.util.concurrent.EventExecutorGroup;
+
+public class EchoClientHandler extends ChannelHandlerAdapter {
+
+    private int counter = 0;
+
+    private final String ECHO_REQ = "Hi, welcome!$_";
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        for (int i = 0; i < 10; i++) {
+            ctx.writeAndFlush(Unpooled.copiedBuffer(ECHO_REQ.getBytes()));
+        }
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        String body = (String) msg;
+        System.out.println("this is " + ++counter + "times receive server: [" + body + "]");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
